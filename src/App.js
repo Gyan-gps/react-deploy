e14dbf92e236a972bfa1bc1b0d52e34fff190ec7
Gyan Prakash Singh <gyangps1998@gmail.com>
2022-08-31 13:49:45 +0530

    hi

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      react-deploy-to-web
+    </div>
+  );
+}
+
+export default App;
